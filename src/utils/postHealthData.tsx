f46c026b2c2446258b65da1e1ed490ad75ea430c
DanStevensonDev <danstevensonco@gmail.com>
2022-09-21 15:56:10 +0100

    update endpoint

--- src/utils/postHealthData.tsx
+++ src/utils/postHealthData.tsx
@@ -7,7 +7,7 @@ const firebaseUrl = process.env.REACT_APP_FIREBASE_URL
 const postHealthData = (healthData: HealthData) => {
     console.log(healthData);
 
-    axios.post(`${firebaseUrl}/test.json`, healthData)
+    axios.post(`${firebaseUrl}/health.json`, healthData)
         .then((response) => {
             console.log(response);
     })
